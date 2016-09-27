99f776e215a6d9972799402bac129a86aab6ac66
Mark Langlois <marklanglois@Marks-MacBook-Pro-2.local>
2016-09-27 11:40:36 -0400

    Added Storyboard lecture 98

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,28 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Mark Langlois on 9/27/16.
+//  Copyright © 2016 Mark Langlois. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl:UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+}
---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Mark Langlois on 9/24/16.
+//  Copyright © 2016 Mark Langlois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+
+        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+    }
+
+
+}
+
