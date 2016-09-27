4aacce8970428e66bbfd54cd6f6d21f698e2793c
Mark Langlois <marklanglois@Marks-MacBook-Pro-2.local>
2016-09-27 17:47:34 -0400

    Added Audio lecture 100

--- pokedex3/ViewController.swift
+++ pokedex3/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
+    
+    var musicPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +24,22 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         
         parsePokemonCSV()
+        initAudio()
         
 }
+    func initAudio() {
+        let path = Bundle.main.path(forResource: "music", ofType: "mp3")
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
     
     func parsePokemonCSV() {
         
@@ -84,4 +101,19 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 }
 
 
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        if musicPlayer.isPlaying {
+            
+            musicPlayer.pause()
+            sender.alpha = 0.5
+            
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+            
+        }
+        
+    }
 }
+    
+
